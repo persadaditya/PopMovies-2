8109a3a8aead21c24aa5f8d21bc2f169b04ad993
Aditya Arief Persada <phedeveng@gmail.com>
2017-08-27 20:22:13 +0700

    POPMovies-2

---
+++ app/src/androidTest/java/com/app/phedev/popmovie/TestUriMatcher.java
@@ -0,0 +1,13 @@
+package com.app.phedev.popmovie;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by phedev in 2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class TestUriMatcher {
+}
